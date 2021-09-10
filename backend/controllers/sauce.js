9f67df1daa880cebd52f2b283080b4f13a59b7fb
Mika <Prisca@mika-2.home>
2021-09-10 10:05:06 +0200

    add backend

---
+++ backend/controllers/sauce.js
@@ -0,0 +1,4 @@
+const Sauce = require('../models/Sauce');
+
+
+
---
+++ backend/routes/sauce.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
+
+const sauceCtrl = require('../controllers/sauce');
+
